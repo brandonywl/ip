76cdf8fbac0706ff3d2fe3f4967865c822b949b8
Brandon <brandonywl@gmail.com>
2020-08-24 17:15:48 +0800

    Followed SE-EDU Java Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,24 +14,24 @@ public class Duke {
         String currentUserInput = "";
         TaskManager taskManager = new TaskManager();
 
-        while (!currentUserInput.equalsIgnoreCase("bye")){
+        while (!currentUserInput.equalsIgnoreCase("bye")) {
             currentUserInput = sc.nextLine();
-            switch (currentUserInput.toUpperCase().split(" ")[0]){
-                case "BYE":
-                    break;
-                case "LIST":
-                    printTasks(taskManager);
-                    break;
-                case "DONE":
-                    int index = Integer.parseInt(currentUserInput.split(" ")[1]);
-                    taskManager.completeTask(index);
-                    Task temp = taskManager.tasks[index - 1];
-                    printMessage(String.format("Nice! I've marked this task as done:\n\t[%s] %s%n", temp.status, temp.job));
-                    break;
+            switch (currentUserInput.toUpperCase().split(" ")[0]) {
+            case "BYE":
+                break;
+            case "LIST":
+                printTasks(taskManager);
+                break;
+            case "DONE":
+                int index = Integer.parseInt(currentUserInput.split(" ")[1]);
+                taskManager.completeTask(index);
+                Task temp = taskManager.tasks[index - 1];
+                printMessage(String.format("Nice! I've marked this task as done:\n\t[%s] %s%n", temp.status, temp.job));
+                break;
 
-                default:
-                    taskManager.addTask(currentUserInput);
-                    printMessage("added: " + currentUserInput);
+            default:
+                taskManager.addTask(currentUserInput);
+                printMessage("added: " + currentUserInput);
             }
         }
 
@@ -39,29 +39,29 @@ public class Duke {
 
     }
 
-    public static void printDivider(){
+    public static void printDivider() {
         String s = "------------------------------------------------";
         System.out.println(s);
     }
 
-    public static void printIntroduction(String logo){
+    public static void printIntroduction(String logo) {
         printDivider();
         System.out.println("Hello I'm \n" + logo);
         System.out.println("What can I do for you?");
         printDivider();
     }
 
-    public static void printExitMessage(){
+    public static void printExitMessage() {
         printMessage("Bye. Hope to see you again soon!");
     }
 
-    public static void printMessage(String s){
+    public static void printMessage(String s) {
         printDivider();
         System.out.println(s);
         printDivider();
     }
 
-    public static void printMessage(String[] s){
+    public static void printMessage(String[] s) {
         printDivider();
         for (String message : s) {
             System.out.println(message);
@@ -69,16 +69,16 @@ public class Duke {
         printDivider();
     }
 
-    public static void printTasks(TaskManager taskManager){
+    public static void printTasks(TaskManager taskManager) {
         Task[] tasks = taskManager.getTasks();
         int numOfTasks = taskManager.getNumberOfTasks();
         String[] messages = new String[numOfTasks];
 
-        if (numOfTasks == 0){
+        if (numOfTasks == 0) {
             System.out.println();
         }
-        for (int i = 0; i < numOfTasks; i++){
-            String outputMessage = String.format("%d.\t[%s] %s", i+1, tasks[i].status, tasks[i].job);
+        for (int i = 0; i < numOfTasks; i++) {
+            String outputMessage = String.format("%d.\t[%s] %s", i + 1, tasks[i].status, tasks[i].job);
             messages[i] = outputMessage;
         }
         printMessage(messages);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,8 +4,12 @@ public class Task {
 
     public Task(String job, boolean status) {
         this.job = job;
-        if (status) complete();
-        else incomplete();
+        if (status){
+            complete();
+        }
+        else{
+            incomplete();
+        }
     }
 
     public Task(String job) {
