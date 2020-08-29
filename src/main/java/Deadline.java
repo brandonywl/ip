151b9e2174f529755ced178fa12d70ac5e7d03b2
brandonywl <brandonywl@gmail.com>
2020-08-30 03:54:58 +0800

    Added Deadline subclass

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,10 @@
+public class Deadline extends Task {
+    protected String deadline;
+
+    public Deadline(String job, String deadline) {
+        super(job);
+        this.deadline = deadline;
+    }
+
+
+}
